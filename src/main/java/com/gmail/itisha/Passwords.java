18c254119319b12d51d70ff433bdef8e0b22d2a1
Tisha1810 <itish65@gmail.com>
2024-10-08 11:25:40 +0300

    Password generator improvements

--- src/main/java/com/gmail/itisha/Passwords.java
+++ src/main/java/com/gmail/itisha/Passwords.java
@@ -1,10 +1,14 @@
 package com.gmail.itisha;
 
+import java.util.Scanner;
+
 import static homeworks.homework231.PasswordGenerator.generatePassword;
 
 public class Passwords {
     public static void main(String[] args) {
-        int passwordLength = 12;
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter password length: ");
+        int passwordLength = scanner.nextInt();
         String generatedPassword = generatePassword(passwordLength);
         System.out.println("Generated password: " + generatedPassword);
     }
